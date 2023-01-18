2b1dfe47031ba2b24946fb5bea8247b8f83d14cc
David <adamsmontoyas19@gmail.com>
2023-01-18 10:51:33 -0500

    Trabajo en clase Semana06

--- EjemplosDos/src/paquete02/DatoTrabajo.java
+++ EjemplosDos/src/paquete02/DatoTrabajo.java
@@ -5,12 +5,31 @@
  */
 package paquete02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class DatoTrabajo {
     // método que permita preguntar al usuario el nombre de la empresa
-    
+
+    // método que permita preguntar al usuario el nombre de la empresa
+     public static String NombreEmpresa(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el nombre de su empresa:");
+        String nombre = entrada.nextLine();
+        
+        return nombre;
+    }
     // método que permita preguntar al usuario la dirección de la empresa
+    public static String DireccionEmpresa(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese la direccion de su empresa:");
+        String direccion = entrada.nextLine();
+        
+        return direccion;
+// método que permita preguntar al usuario la dirección de la empresa
+
+    }
 }
